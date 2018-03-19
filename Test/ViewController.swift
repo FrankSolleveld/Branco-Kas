d8094892c4c88a11c8527a6e90fc7bfca517dfde
Hugocaminada <Hugocaminada@users.noreply.github.com>
2018-03-19 14:50:18 +0100

    -Basic View Controllers aangemaakt en gestyled -Temp pagina stepper en nummer gemaakt

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var label: UILabel!
+    @IBAction func stepper(_ sender: UIStepper) {
+        let temperature = Int(sender.value)
+        label.text = String(temperature) + "°C"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
