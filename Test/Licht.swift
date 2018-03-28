56ba599e74c026a7bdec367f38b66df3e08bd7cb
Frank Solleveld <fsolleveld@outlook.com>
2018-03-28 20:15:49 +0200

    - If you press a button, text below the buttons will match the buttons color.

--- Test/Licht.swift
+++ Test/Licht.swift
@@ -12,6 +12,7 @@ class Licht: UIViewController {
     
     // Link alle knoppen met de code. Check ToggleTheButtons.swift voor de toggle functies. Misschien een IBAction.
 
+    @IBOutlet weak var colorText: UILabel!
     @IBOutlet var toggleButtons : [ToggleTheButtons]! = []
 
     override func viewDidLoad() {
@@ -33,6 +34,38 @@ class Licht: UIViewController {
             if button is UIButton {
                 // Do whatever you want
                _ = button.tag == sender.tag ? (button.layer.borderWidth = 2.0) : (button.layer.borderWidth = 0.0)
+                
+                // Debug tag
+                print(sender.tag)
+                
+                // If button with tag 0 is pressed, text will change to Rood.
+                switch sender.tag   {
+                    
+                case 0:
+                    self.colorText.text="Rood"
+                case 1:
+                    self.colorText.text="Oranje"
+                case 2:
+                    self.colorText.text="Geel"
+                case 3:
+                    self.colorText.text="Groen"
+                case 4:
+                    self.colorText.text="Licht blauw"
+                case 5:
+                    self.colorText.text="Donkerblauw"
+                case 6:
+                    self.colorText.text="Paars"
+                case 7:
+                    self.colorText.text="Roze"
+                case 8:
+                    self.colorText.text="Wit"
+                    
+                default:
+                    self.colorText.text="Kleurloos"
+                }
+                
+//                self.colorText.text=colorOfTheButton
+                
             }
         }
     }
